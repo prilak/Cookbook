04ca62b5f9abd5d506a4820ba2691444b50bfe2d
Michael <mdlawrence@utexas.edu>
2017-08-02 22:59:59 -0500

    commiting in order to sync with cloud 9

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var express = require("express");
 var app = express();
 
+
+
 // home page
 app.get("/", function(req, res){
 	res.send("welcome");
 });
-
-// cookbook 1.0
+//using route variables
+app.get("/culture/:foodName", function(req, res){
+	res.send("welcome to ");
+});
+// keep * here since it will make this the only route
+//this is the default page
+app.get("*", function(req, res){
+	res.send("not welcome");
+});
 
 
 //start server
