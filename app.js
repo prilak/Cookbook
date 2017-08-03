d210135eaa667e0a2a174516a9120b93bf4785d8
Michael Lawrence <mdlawrence@utexas.edu>
2017-08-03 05:06:50 +0000

    syncing repo

--- app.js
+++ app.js
@@ -8,8 +8,15 @@ app.get("/", function(req, res){
 	res.send("welcome");
 });
 //using route variables
-app.get("/culture/:foodName", function(req, res){
-	res.send("welcome to ");
+app.get("/culture/:foodName/:iterations", function(req, res){
+	console.log(req.params.foodName);
+	//res.send("welcome to " + req.params.foodName);
+	var text = "";
+	if(req.params.foodName=="beef") text += "moo ";
+	for(var i = 0; i<req.params.iterations; i++){
+		text += req.params.foodName;
+	}
+	res.send(text);
 });
 // keep * here since it will make this the only route
 //this is the default page
@@ -19,4 +26,4 @@ app.get("*", function(req, res){
 
 
 //start server
-app.listen(3000);
+app.listen(process.env.PORT, process.env.IP);
