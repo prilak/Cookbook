d2d1ef926263d05ad7c426b92e3839e3ee3ed7f9
Michael Lawrence <mdlawrence@utexas.edu>
2017-08-04 06:28:14 +0000

    added a form to create a proto recipe

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
+//This cmd allows us to obtain req for a POST
+app.use(bodyParser.urlencoded({extended: true}));
 
+//this cmd tells the server where the stylesheets are
+app.use(express.static("public"));
 
+//no longer need to add .ejs to views
+app.set("view engine", "ejs");
 // home page
 app.get("/", function(req, res){
 	res.render("home.ejs");
@@ -16,12 +23,34 @@ app.get("/posts", function(req, res) {
     	{title: "is", author: "michael"},
     	{title: "best", author: "michael"}
     ];
-    res.render("posts.ejs", {posts: posts});
-})
+    res.render("posts", {posts: posts});
+});
+
+//dog list app
+app.get("/dogs", function(req, res) {
+    var dogs = ["ziggy", "hennessy", "oreo", "bear", "tiger", "grace"];
+    res.render("dogs", {dogs: dogs});
+});
+
+//adding meals
+//need body-parser to obtain req
+var meals = ["potatoes", "onions"];
+
+app.post("/cookbook/add", function(req, res){
+	var newMeal = req.body.newMeal;
+	meals.push(newMeal);
+	res.redirect("/cookbook/ideas");
+});
+
+app.get("/cookbook/ideas", function(req, res) {
+    res.render("cookbook", {meals: meals});
+});
+
+
 app.get("/:dog", function(req, res) {
     var text = req.params.dog;
-    res.render("love.ejs", {dogName: text});
-})
+    res.render("love", {dogName: text});
+});
 //using route variables
 app.get("/culture/:foodName/:iterations", function(req, res){
 	console.log(req.params.foodName);
@@ -33,6 +62,8 @@ app.get("/culture/:foodName/:iterations", function(req, res){
 	}
 	res.send(text);
 });
+
+
 // keep * here since it will make this the only route
 //this is the default page
 app.get("*", function(req, res){
